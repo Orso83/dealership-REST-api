08efabc2f90067fddeb9b6a3050c47beca2f2c58
Christopher Orsolini <christopher.orsolini@gmail.com>
2020-10-12 18:50:22 -0500

    Moves all files into a new folder, adjust index.php to reflect this.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 
-if ($uri[2] !== 'inventory') {
+if ($uri[3] !== 'inventory') {
     header("HTTP/1.1 404 Not Found");
     exit();
 }
