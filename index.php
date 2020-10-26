d7b90ae3c7193c093e6feb14efefeab04fa2e974
Christopher Orsolini <christopher.orsolini@gmail.com>
2020-10-26 17:16:41 -0500

    Removes uri check for 1and1 hosting.

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode( '/', $uri );
+// $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// $uri = explode( '/', $uri );
 
-if ($uri[3] !== 'inventory') {
-    header("HTTP/1.1 404 Not Found");
-    exit();
-}
+// if ($uri[3] !== 'inventory') {
+//     header("HTTP/1.1 404 Not Found");
+//     exit();
+// }
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
